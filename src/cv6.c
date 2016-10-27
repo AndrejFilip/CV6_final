b04969ac62300b0a4424f9bcaff75bf834a3ef09
AndrejFilip <smejkoTR@gmail.com>
2016-10-26 23:49:03 -0700

    Uloha 1
    Init adc init nvic ... meranie hodnot y potenciometra pomocou prerusenia

---
+++ src/cv6.c
@@ -0,0 +1,81 @@
+/*
+ * cv6.c
+ *
+ *  Created on: Oct 26, 2016
+ *      Author: Andrej
+ */
+#include "cv6.h"
+#include <stddef.h>
+#include "stm32l1xx.h"
+#include <stdio.h>
+#include <string.h>
+
+
+void init_adc(void)
+{
+ GPIO_InitTypeDef GPIO_InitStructure;
+ ADC_InitTypeDef ADC_InitStructure;
+ /* Enable GPIO clock */
+ RCC_AHBPeriphClockCmd(GPIOB, ENABLE);//Opraviť a upraviť
+ /* Configure ADCx Channel 2 as analog input */
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 ;
+ GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+ GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+ GPIO_Init(GPIOA, &GPIO_InitStructure);
+/* Enable the HSI oscillator */
+ RCC_HSICmd(ENABLE);
+/* Check that HSI oscillator is ready */
+ while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
+ /* Enable ADC clock */
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+ /* Initialize ADC structure */
+ ADC_StructInit(&ADC_InitStructure);
+ /* ADC1 configuration */
+ ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+ ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+ ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+ ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+ ADC_InitStructure.ADC_NbrOfConversion = 1;
+ ADC_Init(ADC1, &ADC_InitStructure);
+/* ADCx regular channel8 configuration */
+ ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 1, ADC_SampleTime_96Cycles);
+
+ /* Enable the ADC */
+ init_NVIC();
+ ADC_Cmd(ADC1, ENABLE);
+
+
+ /* Wait until the ADC1 is ready */
+ while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET)
+ {
+ }
+ /* Start ADC Software Conversion */
+ ADC_SoftwareStartConv(ADC1);
+
+
+
+
+
+
+}
+void init_NVIC(void){
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = ADC1_IRQn; //zoznam prerušení nájdete v súbore stm32l1xx.h
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    ADC_ITConfig(ADC1, ADC_IT_EOC, ENABLE);
+
+}
+
+uint16_t value = 0;
+
+void  ADC1_IRQHandler(void){
+    if(ADC1->SR & ADC_SR_EOC){
+
+        value = ADC1->DR;
+
+    }
+}
---
+++ src/cv6.h
@@ -0,0 +1,15 @@
+/*
+ * cv6.h
+ *
+ *  Created on: Oct 26, 2016
+ *      Author: Andrej
+ */
+
+#ifndef CV6_H_
+#define CV6_H_
+void init_adc(void);
+void init_NVIC(void);
+void ADC1_IRQHandler(void);
+
+
+#endif /* CV6_H_ */
